cc239a50195aa1854acb9604f59c144cb0c58a97
Peter Mohos <peter.mohos@balabit.com>
2018-11-24 13:12:56 +0100

    digit load and image context

---
+++ recognition/src/image_context.py
@@ -0,0 +1,26 @@
+from cv2.cv2 import cvtColor, COLOR_BGR2GRAY, GaussianBlur, Canny
+
+
+class ImageContext:
+
+    def __init__(self, image):
+        self._image = image
+        self._gray_image = cvtColor(image, COLOR_BGR2GRAY)
+        self._blurred_image = GaussianBlur(self._gray_image, (5, 5), 0)
+        self._edged_image = Canny(self._blurred_image, 50, 200, 255)
+
+    @property
+    def image(self):
+        return self._image
+
+    @property
+    def gray_image(self):
+        return self._gray_image
+
+    @property
+    def blurred_image(self):
+        return self._blurred_image
+
+    @property
+    def edged_image(self):
+        return self._edged_image
---
+++ recognition/src/utils.py
@@ -0,0 +1,19 @@
+import os
+from json import load
+
+DIGITS_PATH = os.path.abspath(
+    os.path.join(
+        [os.path.dirname(os.path.abspath(__file__)), "assets", "digits.json"]
+    )
+)
+
+
+def _load_digits():
+    return load(DIGITS_PATH)
+
+
+def _get_digit_map(digit_json: dict):
+    return {
+        tuple(value): idx
+        for idx, value in enumerate(digit_json)
+    }
