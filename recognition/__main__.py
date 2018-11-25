ea0a7d7239784e89d992fb4f1ecd92641401df29
Peter Mohos <peter.mohos@balabit.com>
2018-11-25 01:33:49 +0100

    meh: exception

--- recognition/__main__.py
+++ recognition/__main__.py
@@ -7,11 +7,15 @@ from src.image_utils import get_contours_of_image, \
 from src.utils import get_digit_map, load_image
 
 if __name__ == "__main__":
-    arg_parser = ArgumentParser()
-    arg_parser.add_argument("image_path", type=str, help="Path of the image with numbers")
-    args = arg_parser.parse_args()
-    image_context = ImageContext(load_image(args.image_path))
+    try:
+        arg_parser = ArgumentParser()
+        arg_parser.add_argument("image_path", type=str, help="Path of the image with numbers")
+        args = arg_parser.parse_args()
+        image_context = ImageContext(load_image(args.image_path))
 
-    digitCnts = get_digit_contours(get_contours_of_image(image_context.image))
-    digits = [str(digit) for digit in get_digits_from_digit_contours(get_digit_map(), digitCnts, image_context.image)]
-    print(",".join(digits))
+        digitCnts = get_digit_contours(get_contours_of_image(image_context.image))
+        digits = [str(digit) for digit in get_digits_from_digit_contours(get_digit_map(), digitCnts, image_context.image)]
+        print(",".join(digits))
+        exit()
+    except Exception:
+        exit(1)
