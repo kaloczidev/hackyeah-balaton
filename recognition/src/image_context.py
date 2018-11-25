417ac760a6aa5c959c6467c26b79f7015be412c9
Peter Mohos <peter.mohos@balabit.com>
2018-11-25 11:27:26 +0100

    Revert "meh: final meh"
    This reverts commit 724afa3fc310b1584c30ca60f8b6e8c0ede141ed.

--- recognition/src/image_context.py
+++ recognition/src/image_context.py
@@ -1,23 +1,9 @@
-import numpy
-from cv2 import cvtColor, COLOR_BGR2GRAY, GaussianBlur, THRESH_BINARY, threshold, cv2
+from cv2 import cvtColor, COLOR_BGR2GRAY, GaussianBlur, THRESH_BINARY, threshold
 
 
 class ImageContext:
 
     def __init__(self, image, g_h, g_w, tr):
-        kernel = numpy.ones((5, 5), numpy.uint8)
-
-        img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-        img = img[120:120 + 60, 64:64 + 248]
-
-        img = cv2.erode(img, kernel, iterations=1)
-
-        img = cv2.dilate(img, kernel, iterations=1)
-
-        img = cv2.GaussianBlur(img, (7, 7), 0)
-
-        _, img = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY_INV)
         _, self.image = threshold(
             GaussianBlur(
                 cvtColor(image, COLOR_BGR2GRAY),
